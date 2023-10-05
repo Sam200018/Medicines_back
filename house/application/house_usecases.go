654f8505a08ead6d2d319b89bcec419b2f5159fa
Samuel Bautista <samuelbaubas@gmail.com>
2023-10-05 17:00:36 -0600

    house use cases implemented

---
+++ house/application/house_usecases.go
@@ -0,0 +1,27 @@
+package application
+
+import "github.com/Samuel200018/pills_backend/house/domain"
+
+type HouseUseCase struct {
+	homeRepo domain.HouseRepository
+}
+
+func NewNewCasesHouse(homeRepo domain.HouseRepository) *HouseUseCase {
+	return &HouseUseCase{homeRepo}
+}
+
+func (huc *HouseUseCase) CreateHouse(house domain.House) (domain.House, error) {
+	return huc.homeRepo.Create(house)
+}
+
+func (huc *HouseUseCase) GetHouseById(id string) (domain.House, error) {
+	return huc.homeRepo.GetById(id)
+}
+
+func (huc *HouseUseCase) UpdateHouse(house domain.House) (domain.House, error) {
+	return huc.homeRepo.Update(house)
+}
+
+func (huc *HouseUseCase) DeleteHouse(id string) error {
+	return huc.homeRepo.Delete(id)
+}
