288ec2b8f56c03aaf53d0641a4ef5a7d243d4470
Samuel Bautista <samuelbaubas@gmail.com>
2023-10-11 18:46:35 -0600

    house handler updated

--- house/house_handler.go
+++ house/house_handler.go
@@ -5,7 +5,9 @@ import (
 	authApp "github.com/Samuel200018/pills_backend/auth/application"
 	"github.com/Samuel200018/pills_backend/house/application"
 	"github.com/Samuel200018/pills_backend/house/domain"
+	"github.com/gorilla/mux"
 	"net/http"
+	"strconv"
 )
 
 type HouseHandler struct {
@@ -42,4 +44,70 @@ func (houseHad *HouseHandler) CreateHouse(w http.ResponseWriter, r *http.Request
 		"user_updated": userUpdated,
 	}
 	json.NewEncoder(w).Encode(response)
+}
+
+func (houseHad *HouseHandler) JoinHouse(w http.ResponseWriter, r *http.Request) {
+	houseFromRequest := mux.Vars(r)["house-id"]
+	parseUint, err := strconv.ParseUint(houseFromRequest, 10, 32)
+	if err != nil {
+		http.Error(w, "Error parsing string", http.StatusBadRequest)
+		return
+	}
+	houseId := uint(parseUint)
+	userId := r.FormValue("user_id")
+
+	userUpdated, err := houseHad.authUsesCases.AddHouse(userId, houseId)
+	if err != nil {
+		http.Error(w, "Error joining to house", http.StatusBadRequest)
+		return
+	}
+
+	response := map[string]interface{}{
+		"message":      "House created successfully",
+		"user_updated": userUpdated,
+	}
+	json.NewEncoder(w).Encode(response)
+
+}
+
+func (houseHad *HouseHandler) ExitHouse(w http.ResponseWriter, r *http.Request) {
+	houseFromRequest := mux.Vars(r)["house-id"]
+	parseUint, err := strconv.ParseUint(houseFromRequest, 10, 32)
+	if err != nil {
+		http.Error(w, "Error parsing string", http.StatusBadRequest)
+		return
+	}
+	houseId := uint(parseUint)
+	userId := r.FormValue("user_id")
+
+	countUsers, err := houseHad.authUsesCases.UsersWithHouseId(houseId)
+	if err != nil {
+		http.Error(w, "Error counting users with house id", http.StatusBadRequest)
+		return
+	}
+
+	if countUsers > 1 {
+		//	Eliminar house\
+		err := houseHad.houseUseCases.DeleteHouse(houseFromRequest)
+		if err != nil {
+			http.Error(w, "Error deleting house", http.StatusBadRequest)
+			return
+		}
+
+	}
+
+	userUpdated, err := houseHad.authUsesCases.ExitHouse(userId)
+
+	if err != nil {
+		print(err)
+		http.Error(w, "User not exit the house", http.StatusBadRequest)
+		return
+	}
+
+	response := map[string]interface{}{
+		"message":      "Exit house successfully",
+		"user_updated": userUpdated,
+	}
+	json.NewEncoder(w).Encode(response)
+
 }
